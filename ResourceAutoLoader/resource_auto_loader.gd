9af6077b6b3a67fdec153f90307cf20c5951bb16
Guillem Chesa <fr.chesa@gmail.com>
2025-08-11 00:44:38 +0200

    Added inspector_plugin and property checks

--- ResourceAutoLoader/resource_auto_loader.gd
+++ ResourceAutoLoader/resource_auto_loader.gd
@@ -2,6 +2,16 @@
 class_name ResourceAutoLoader extends EditorPlugin
 ## Allows loading resources dynamically. Use the [code]load(Node, Object)[/code] method.
 
+var inspector_plugin
+
+
+func _enter_tree():
+	inspector_plugin = preload("inspector_plugin.gd").new()
+	add_inspector_plugin(inspector_plugin)
+
+func _exit_tree():
+	remove_inspector_plugin(inspector_plugin)
+	
 
 ## Searches for an Array variable named [code]resources[/code] in the owner node of [code]object[/code].
 ## Then it finds any resource that extends [code]resoure_type[/code] and loads any exported variables
@@ -21,25 +31,53 @@ static func load(object : Node, resource_type : Object) -> void:
 		return
 		
 	# Gets all the properties from the resource and the object
-	var resource_properties = resource.get_property_list()
-	var object_properties = _get_property_names(object.get_property_list())
+	var resource_properties = _get_resource_properties(resource)
+	var object_properties = _get_object_properties(object)
 	
 	for prop in resource_properties:
-		
-		# If the resource property is exported and the object has the same property, sets the object property with the value of the resource
-		if prop.usage == 4102:
-			var name = prop.name
-			if object_properties.has(name):
-				object.set(name, resource.get(name))
-				print(prop.name)
+		if object_properties.has(prop):
+			object.set(prop, resource.get(prop))
 
 
 ## Private method for extracting only the names of the properties after using the [code]Object.get_property_list()[/code] method.
-static func _get_property_names(property_list : Array) -> Array:
+static func _get_object_properties(object : Node) -> Array:
 	
 	var property_names := []
 	
-	for prop in property_list:
-		property_names.append(prop.name)
+	for p in object.get_property_list():
+		property_names.append(p.name)
 		
 	return property_names
+	
+	
+## Private method for extracting the properties from the Resource ignoring properties that shouldn't be replaced in the object
+static func _get_resource_properties(resource : Resource) -> Array:
+	
+	# Adds any base property to a list
+	var base_properties = [
+		"RefCounted", "Resource", "resource_local_to_scene", "resource_path",
+		"resource_name", "resource_scene_unique_id", "script", "metadata/_custom_type_script"
+	]
+
+	for p in ClassDB.class_get_property_list("Resource"):
+		base_properties.append(p.name)
+		
+	for p in ClassDB.class_get_property_list("Script"):
+		base_properties.append(p.name)
+		
+	for p in ClassDB.class_get_property_list("Node"):
+		base_properties.append(p.name)
+		
+	for p in ClassDB.class_get_property_list("Object"):
+		base_properties.append(p.name)
+
+
+	# Adds the Resource properties that are not in the base properties
+	var custom_properties = []
+	for p in resource.get_property_list():
+		if p.name not in base_properties:
+			custom_properties.append(p.name)
+			
+
+	return custom_properties
+	
