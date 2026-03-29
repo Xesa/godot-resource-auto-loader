facba97da0d1c60852fcd2b49a7d55fd6baff594
Guillem Chesa <fr.chesa@gmail.com>
2026-03-29 17:49:17 +0200

    feat(config-loader): Added config loader logic and debug flag

--- InspectorPlugin/resource_auto_loader.gd
+++ InspectorPlugin/resource_auto_loader.gd
@@ -2,18 +2,31 @@
 class_name ResourceAutoLoader extends EditorPlugin
 ## Allows loading resources dynamically. Use the [code]load(Node, Object)[/code] method.
 
-const RESOURCE_TYPE_NAME := "resource_type"
+static var RESOURCE_TYPE_NAME := "resource_type"
+static var DEBUG := false
 
 var inspector_plugin
 
 func _enter_tree():
-	inspector_plugin = preload("inspector_plugin.gd").new()
-	add_inspector_plugin(inspector_plugin)
+	var err = check_config_file()
+	
+	if err == OK:
+		inspector_plugin = preload("inspector_plugin.gd").new()
+		add_inspector_plugin(inspector_plugin)
 
 func _exit_tree():
 	remove_inspector_plugin(inspector_plugin)
 
 
+## Checks and loads the plugin configuration file.
+func check_config_file() -> int:
+	var config := ConfigFile.new()
+	var err := config.load("res://addons/ResourceAutoLoader/plugin.cfg")
 
+	if err == OK:
+		RESOURCE_TYPE_NAME = config.get_value("plugin", "resource_type_name")
+		DEBUG = config.get_value("plugin", "debug")
+	else:
+		printerr("Resource Auto Loader: Error loading .cfg file. Please, reinstall the plugin")
 
-	
+	return err
