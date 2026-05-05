cd579ea7deb835853e9cc65812741723a6aa0309
Guillem Chesa <fr.chesa@gmail.com>
2026-05-05 20:19:02 +0200

    feat(hint-system): Changed property search and creation methods for the new hint system.

--- InspectorPlugin/inspector_plugin.gd
+++ InspectorPlugin/inspector_plugin.gd
@@ -1,6 +1,17 @@
 @tool
 extends EditorInspectorPlugin
 
+const MAIN := preload("main.gd")
+const HELPERS := preload("helpers.gd")
+
+const CHECKBOX_PROPERTY := preload("../EditorProperties/checkbox_property.gd")
+const NODEPATH_PROPERTY := preload("../EditorProperties/nodepath_property.gd")
+const NUMBER_PROPERTY := preload("../EditorProperties/number_property.gd")
+const RESOURCE_PROPERTY := preload("../EditorProperties/resource_property.gd")
+const STRING_PROPERTY := preload("../EditorProperties/string_property.gd")
+const VECTOR2_PROPERTY := preload("../EditorProperties/vector2_property.gd")
+const VECTOR3_PROPERTY := preload("../EditorProperties/vector3_property.gd")
+
 
 func _can_handle(object: Object) -> bool:
 	if object is not Node or object is EditorProperty:
@@ -11,43 +22,53 @@ func _can_handle(object: Object) -> bool:
 			
 func _parse_begin(object : Object) -> void:
 
-	# Iterates every child and checks if any of them includes an exportable resource
-	for child : Node in object.get_children():
-		var resource_type : Object = child.get(ResourceAutoLoader.RESOURCE_TYPE_NAME)
+	# Returns if the selected node is not an importer
+	if HELPERS.get_object_property(object, MAIN.IMPORTER_NODE_FLAG).size() == 0:
+		return
+
+	# Iterates every child and finds their exportable properties
+	var properties := HELPERS.scan_children_nodes(object)
+
+	for node_info in properties:
+		_add_property_editors(node_info)
+
+
+func _add_property_editors(node_info : Dictionary) -> void:
+
+	var node : Node = node_info["node"]
+	var node_name : String = node_info["node_name"]
+	var properties : Dictionary = node_info["properties"]
+
+	if properties.size() == 0:
+		return
+
+	HELPERS.create_group_label(self, node.name)
+
+	# Iterates every property from the exportable resource and checks if the current object has them
+	for name in properties.keys():
+
+		if MAIN.DEBUG:
+			print("Property %s (%d)" % [name, properties[name].type])
+
+		var editor_property : EditorProperty
 		
-		# Creates a group label for each child that includes an exportable resource
-		if resource_type is Resource:
-			ResourceAutoLoaderHelpers.create_group_label(self, child.name)
+		# If it's a basic property, creates a custom PropertyEditor for that property
+		match properties[name].type:
+			1: editor_property = CHECKBOX_PROPERTY.new(node, properties[name])
+			2: editor_property = NUMBER_PROPERTY.new(node, properties[name])
+			3: editor_property = NUMBER_PROPERTY.new(node, properties[name])
+			4: editor_property = STRING_PROPERTY.new(node, properties[name])
+			5: editor_property = VECTOR2_PROPERTY.new(node, properties[name])
+			9: editor_property = VECTOR3_PROPERTY.new(node, properties[name])
 
-			var child_properties := ResourceAutoLoaderHelpers._get_object_properties(child)
-			var resource_properties := ResourceAutoLoaderHelpers._get_resource_type_properties(resource_type.new())
+			24: 
+				if properties[name].hint_string == MAIN.EXPORTABLE_NODEPATH_HINT:
+					editor_property = NODEPATH_PROPERTY.new(node, properties[name])
+				else:
+					editor_property = RESOURCE_PROPERTY.new(node, properties[name])
 
-			# Iterates every property from the exportable resource and checks if the current object has them
-			for name in resource_properties.keys():
-
-				if ResourceAutoLoader.DEBUG:
-					print("Property %s (%d / %d)" % [name, resource_properties[name].type, child_properties[name].type])
-
-				if child_properties.has(name):
-					var editor_property : EditorProperty
-					
-					# If it's a basic property, creates a custom PropertyEditor for that property
-					if resource_properties[name].type == child_properties[name].type:
-						match resource_properties[name].type:
-							1: editor_property = ResourceAutoLoaderCheckbox.new(child, resource_properties[name])
-							2: editor_property = ResourceAutoLoaderNumber.new(child, resource_properties[name])
-							3: editor_property = ResourceAutoLoaderNumber.new(child, resource_properties[name])
-							4: editor_property = ResourceAutoLoaderString.new(child, resource_properties[name])
-							5: editor_property = ResourceAutoLoaderVector2.new(child, resource_properties[name])
-							9: editor_property = ResourceAutoLoaderVector3.new(child, resource_properties[name])
-							24: editor_property = ResourceAutoLoaderResource.new(child, resource_properties[name])
-							_: continue
-
-					# If it's a NodePath, creates a custom PropertyEditor for managing node paths
-					elif resource_properties[name].type == 22 and child_properties[name].type == 24:
-						editor_property = ResourceAutoLoaderNodepath.new(child, resource_properties[name])
-					
-					# Finally, adds the PropertyEditor to the UI
-					if editor_property:
-						add_property_editor(name, editor_property, true, name.capitalize())
-						
+			_: continue
+		
+		# Finally, adds the PropertyEditor to the UI
+		if editor_property:
+			add_property_editor(name, editor_property, true, name.capitalize())
